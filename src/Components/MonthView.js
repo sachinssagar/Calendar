de46a5c91050cc2f22dc7bc055613cc1c7b0fe55
Sachin Sagar <sachinssagar@gmail.com>
2023-10-07 15:55:51 +0530

    Complete

--- src/Components/MonthView.js
+++ src/Components/MonthView.js
@@ -1,8 +1,123 @@
-// MonthView.js
-import React from "react";
+import React, { useState } from "react";
+import "./MonthCalendar.css";
 
-const MonthView = () => {
-  return <div>Month View</div>;
+const MonthCalendar = () => {
+  const [year, setYear] = useState("");
+  const [month, setMonth] = useState("");
+  const [calendar, setCalendar] = useState([]);
+  const [showCalendar, setShowCalendar] = useState(false); // Initially hide the calendar
+  const [monthError, setMonthError] = useState(""); // State to hold validation error
+
+  const handleYearChange = (e) => {
+    setYear(e.target.value);
+  };
+
+  const handleMonthChange = (e) => {
+    const selectedMonth = e.target.value;
+    if (selectedMonth >= 1 && selectedMonth <= 12) {
+      setMonth(selectedMonth);
+      setMonthError(""); // Clear any previous validation error
+    } else {
+      setMonth(""); // Clear the month if it's out of range
+      setMonthError("Month must be between 1 and 12");
+    }
+  };
+
+  const generateMonthCalendar = () => {
+    const calendarsArray = [];
+    const date = new Date(year, month - 1, 1); // Subtract 1 from month since it's zero-based
+    const monthName = date.toLocaleString("default", { month: "long" });
+
+    for (let i = 0; i < 1; i++) {
+      // You can modify this loop to display multiple months if needed
+      const monthCalendar = generateMonthTable(date);
+
+      calendarsArray.push(
+        <div key={i} className="month-calendar">
+          <h2>{monthName}</h2>
+          {monthCalendar}
+        </div>
+      );
+
+      date.setMonth(date.getMonth() + 1); // Move to the next month
+    }
+
+    setCalendar(calendarsArray);
+    setShowCalendar(true);
+  };
+
+  const generateMonthTable = (date) => {
+    const daysInMonth = [];
+    const totalDays = new Date(
+      date.getFullYear(),
+      date.getMonth() + 1,
+      0
+    ).getDate();
+
+    const startDayOfWeek = new Date(
+      date.getFullYear(),
+      date.getMonth(),
+      1
+    ).getDay();
+
+    for (let i = 0; i < startDayOfWeek; i++) {
+      daysInMonth.push(<td key={`empty-${i}`} className="empty-cell"></td>);
+    }
+
+    for (let day = 1; day <= totalDays; day++) {
+      daysInMonth.push(<td key={day}>{day}</td>);
+
+      if ((day + startDayOfWeek) % 7 === 0 || day === totalDays) {
+        daysInMonth.push(<tr key={`week-${day}`} />);
+      }
+    }
+
+    return (
+      <table>
+        <thead>
+          <tr>
+            <th>Sun</th>
+            <th>Mon</th>
+            <th>Tue</th>
+            <th>Wed</th>
+            <th>Thu</th>
+            <th>Fri</th>
+            <th>Sat</th>
+          </tr>
+        </thead>
+        <tbody>{daysInMonth}</tbody>
+      </table>
+    );
+  };
+
+  return (
+    <div className="month-calendar">
+      <h1>Month Calendar</h1>
+      <div className="input-container">
+        <label htmlFor="year">Enter the year:</label>
+        <input
+          type="number"
+          placeholder="Year"
+          value={year}
+          onChange={handleYearChange}
+        />
+      </div>
+      <div className="input-container">
+        <label htmlFor="month">Enter the month:</label>
+        <input
+          type="number"
+          placeholder="Month"
+          value={month}
+          onChange={handleMonthChange}
+        />
+      </div>
+      {monthError && <div className="error">{monthError}</div>}{" "}
+      {/* Display validation error if any */}
+      <button onClick={generateMonthCalendar}>Submit</button>
+      {showCalendar && calendar}{" "}
+      {/* Display the calendar if showCalendar is true */}
+    </div>
+  );
 };
 
-export default MonthView;
+export default MonthCalendar;
